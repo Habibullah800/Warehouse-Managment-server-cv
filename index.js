5a46ac68583403ceea036a738d2fab32cb022c3c
Habibullah800 <mdhabibullah800@gmail.com>
2022-05-08 13:50:54 +0600

    add item api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-//require('dotenv').config();
+require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
 
@@ -12,15 +12,35 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgdai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('wareHouseManage').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await serviceCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
 
 
 
