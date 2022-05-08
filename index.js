c39350f96bf22862d5cf8fc0b39fed054a22d517
Habibullah800 <mdhabibullah800@gmail.com>
2022-05-08 14:59:58 +0600

    Delete api cteate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
@@ -33,6 +33,16 @@ async function run() {
             const result = await serviceCollection.insertOne(newProduct);
             res.send(result);
         })
+
+        // Delete Api 
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result);
+        })
+
+
 
 
     }
