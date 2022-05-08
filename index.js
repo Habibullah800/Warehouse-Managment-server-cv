040213ccc284702f7dad581c03e4ba8c0e6bcc05
Habibullah800 <mdhabibullah800@gmail.com>
2022-05-08 20:04:01 +0600

    clean index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 //Using for middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgdai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -42,19 +39,12 @@ async function run() {
             res.send(result);
         })
 
-
-
-
     }
     finally {
 
     }
 };
 run().catch(console.dir);
-
-
-
-
 
 app.get('/', (req, res) => {
     res.send('running my node mongo')
